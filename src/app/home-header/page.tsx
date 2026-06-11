45e2e273c26818117e638e27a21091f99e11a10e
Seoje1405 <hipop99@naver.com>
2026-06-12 00:01:51 +0900

    feat: 홈 헤더 컴포넌트 구현.

---
+++ src/app/home-header/page.tsx
@@ -0,0 +1,9 @@
+import { AppHeader } from '@/components/layout/AppHeader';
+
+export default function HomeHeaderPage() {
+  return (
+    <div className="fixed top-0 w-full">
+      <AppHeader />
+    </div>
+  );
+}
