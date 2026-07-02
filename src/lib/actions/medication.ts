9c30f370790fb8b1dec0fa4491d31f84e0cdbeb7
Seoje1405 <hipop99@naver.com>
2026-07-03 00:17:04 +0900

    feat: medication 서버 액션 및 날짜 유틸리티 추가
    - src/lib/actions/medication.ts: 복약 관련 서버 액션
    - src/lib/date.ts: 날짜 처리 유틸리티

---
+++ src/lib/actions/medication.ts
@@ -0,0 +1,5 @@
+'use server';
+
+// TODO: API 연동 시 실제 DB 업데이트 후 revalidatePath('/home') 호출로 교체
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export async function checkMedication(_medId: string, _checked: boolean): Promise<void> {}
